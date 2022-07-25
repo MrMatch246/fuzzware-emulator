9c9cc1f74e836bd932d1a36d9d169d255a7ecd73
MrMatch246 <s8daerce@stud.uni-saarland.de>
2022-07-25 22:53:24 +0200

    Moved from python3 to pypy3

--- setup.sh
+++ setup.sh
@@ -9,15 +9,15 @@ make -C AFLplusplus clean all || {
 }
 
 echo "[*] Building afl and Unicorn"
-UNICORN_QEMU_FLAGS="--python=/usr/bin/python3" make -C afl clean all || exit 1
+UNICORN_QEMU_FLAGS="--python=/usr/bin/pypy3" make -C afl clean all || exit 1
 pushd unicorn; USERNAME=`whoami` ./build_unicorn.sh || { popd; exit 1; }; popd
 
 echo "[*] Building native harness module"
 make -C harness/fuzzware_harness/native clean all || exit 1
 
 echo "[*] Installing harness"
-pip3 install -U cython || exit 1
-pip3 install -U -r requirements.txt || exit 1
+pypy3 -m pip install -U cython || exit 1
+pypy3 -m pip install -U -r requirements.txt || exit 1
 
-pushd harness; pip3 install -e . || { popd; exit 1; }; popd
+pushd harness; pypy3 -m pip install -e . || { popd; exit 1; }; popd
 exit 0
